6565f1df493a82c9d6ffaa99c03f2d06992d40ae
Mustafa <cana66331@gmail.com>
2021-05-01 11:24:43 +0200

    console.log deleted

--- etch-a-sketch/myScript.js
+++ etch-a-sketch/myScript.js
@@ -22,12 +22,11 @@ updateGrid = () => {
   );
   if((userInput.value<=100)&&(userInput.value>0)){
        for (let i = 0; i < userInput.value * userInput.value; i++) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    grid.appendChild(div);
+        const div = document.createElement("div");
+        div.classList.add("square");
+        grid.appendChild(div);
     }
   }
-  console.log(userInput.value);
 };
 
 const square = document.querySelector("div");
