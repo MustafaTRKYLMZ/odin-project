307d23a7ca33cf174058ba3c943638e3899a2af6
Mustafa <cana66331@gmail.com>
2021-05-01 11:01:00 +0200

    etch-a-sketch finished

--- etch-a-sketch/myScript.js
+++ etch-a-sketch/myScript.js
@@ -1,40 +1,48 @@
+const grid = document.querySelector(".gridContainer");
+const userInput = document.getElementById("inputNumber");
+const resetButton = document.querySelector(".reset");
 
-window.addEventListener('click', getInputValue);
-
-function getInputValue(e){
- 
-let inputValue = document.getElementById('inputNumber').value//input value
-console.log("input value",inputValue)
-
-
-
-
-//let newGrid = document.createAttribute("class")
-
-                
-let marginCount=10;
-for(i=0;i<5;i++){
-    let gridArea = document.getElementById("newGrid")
-var attrResult = document.createAttribute("class");
-attrResult.value = "grid-container";
-gridArea.style.margin=`${marginCount}px`//${marginCount}"px
-marginCount =marginCount+10;
-    gridArea.setAttributeNode(attrResult);
-    console.log("i =>",i)
-    console.log("Margin Count",marginCountva)
-}
-
-/*
-let grid = document.createElement("div")
-grid.className="grid-container"
-gridArea.innerText="Hello"
-gridArea.classList.add(grid)
-console.log("gridArea",gridArea)
-*/
-}
-
-function mouseOver() {
-    document.getElementById("demo").style.color = "red";
-    console.log("mouse over")
+createGrid = () => {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
   }
-  
+};
+
+updateGrid = () => {
+  grid.innerHTML = "";
+  grid.style.setProperty(
+    "grid-template-columns",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  grid.style.setProperty(
+    "grid-template-rows",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  if((userInput.value<=100)&&(userInput.value>0)){
+       for (let i = 0; i < userInput.value * userInput.value; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+    }
+  }
+  console.log(userInput.value);
+};
+
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function(event) {
+  event.target.classList.replace("square", "color");
+});
+
+userInput.addEventListener("change", updateGrid);
+
+resetButton.addEventListener("click", function() {
+  grid.innerHTML = "";
+  userInput.value = "";
+  grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+  grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+  createGrid();
+});
+
+createGrid();
