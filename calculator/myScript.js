f021e07de5b284f1bdaa12e35262d06ae54f40a5
Mustafa <cana66331@gmail.com>
2021-05-22 20:57:42 +0200

    calculator finished

--- calculator/myScript.js
+++ calculator/myScript.js
@@ -1,113 +1,145 @@
-// All process (+ - * / %) is working effectively. 
-//You can contionue other functions
-
-//ASSİGNMENTS
-//1- you should Result Area2 ad Result Area1
-// 2- you should built CC process
-// 3- sumAll is continue out of the result Area3. You should fix them. 
 
 const first = document.getElementById("firstResult");
-const equal = document.getElementById("equal").value;
+const second = document.getElementById("secondResult");
+const third = document.getElementById("thirdResult");
 let sumAll ="";
 let sumCount="";
+let sumLast ="";
 
 const handleClick=(id)=>{
-   console.log("SumCount",sumCount)
-switch(id.value) {
-   case "clear":
-     document.getElementById("firstResult").innerHTML = "";
-     sumAll=""
-     sumCount=""
-     break;
-   case "plus":
-     sumAll += "+";
-     sumCount +="+";
-     document.getElementById("firstResult").innerHTML = sumAll;
-  
-     break;
-   case "minus":
-      sumAll += "-";
-      sumCount +="-";
-      document.getElementById("firstResult").innerHTML = sumAll;
-      break;
-   case "multiply":
-      sumAll += "*";
-     sumCount +="*";
-     document.getElementById("firstResult").innerHTML = sumAll;
-      break;
-   case 'divide':
-      sumAll += "/";
-     sumCount +="/";
-     document.getElementById("firstResult").innerHTML = sumAll;
-      break;
-      case 'percent':
-         sumAll += "%";
-        sumCount +="%";
-        document.getElementById("firstResult").innerHTML = sumAll;
-         break;
-   case "equal":
-      let result=0;
-      let newArray=[];
-      if(sumCount.includes("+")){
-         newArray = sumCount.split("+")
-         result = process(newArray,"+");
-      } else if(sumCount.includes("-")){
-         newArray = sumCount.split("-");
-         result = process(newArray,"-");
-
-      } else if(sumCount.includes("*")){
-         newArray = sumCount.split("*");
-         result = process(newArray,"*");
-
-      } else if(sumCount.includes("/")){
-         newArray = sumCount.split("/");
-         result = process(newArray,"/");
-
-      } else if(sumCount.includes("%")){
-         newArray = sumCount.split("%");
-         result = process(newArray,"%");
-
-      }
-    
-    console.log("Result",result)
-    sumAll +="="+result;
-    
-   document.getElementById("firstResult").innerHTML = sumAll;
-   sumCount=result;
-   console.log("sumCount",sumCount)
-   console.log("sumAll",sumAll)
-      break;
-   
-
-  
-   default:
-     // code block
-    sumAll += id.value;
-    sumCount +=id.value;//parseInt
-    document.getElementById("firstResult").innerHTML = sumAll;
- }
-
- 
-   
-  
+  operate(id);
 }
-
+const setField = (sumLast,sumAll) => {
+   first.innerHTML = sumAll;
+   second.innerHTML=sumLast
+   if(sumLast.length>50){
+      newlast= sumLast.substring(sumLast.length-51,sumLast.length)
+      newThird = sumLast.substring(sumLast.length-102,sumLast.length-51)
+      second.innerHTML=newlast;
+      third.innerHTML = newThird;
+   }
+}
 const process=(array,process)=>{
-   console.log("in process",array)//array =["9","1"]
-sum=0;
-//for(i=0;i<array.length;i++){
+   sum=0;
+   if(array.length===1){
+      sum = parseInt(array[0])
+      return sum;
+   }
    if(process ==="+"){
-        sum = parseFloat(array[0])+parseFloat(array[1])
+      sum = parseFloat(sumCount)+parseFloat(array[1])
    } else if(process === "-"){
-      sum = parseFloat(array[0])-parseFloat(array[1]);
-   } else if(process === "*"){
-      sum = parseFloat(array[0])*parseFloat(array[1]);
+      sum = parseFloat(sumCount)-parseFloat(array[1]);
+   } else if(process === "x"){
+      sum = parseFloat(sumCount)*parseFloat(array[1]);
    } else if(process === "/"){
-      sum = parseFloat(array[0])/parseFloat(array[1]);
+      sum = parseFloat(sumCount)/parseFloat(array[1]);
    }  else if(process === "%"){
-    
-      sum = parseFloat(array[0])%parseFloat(array[1]);
+      sum = parseFloat(sumCount)%parseFloat(array[1]);
    } 
-//};
-return sum;
+   return sum;
  };
+const operatorProcess=(operator)=>{
+   sumAll += operator;
+   sumLast +=operator;
+   setField(sumLast,sumAll)
+    let arrayOne = sumAll.substring(0,sumAll.length);
+    let array = arrayOne.split(operator)
+    let newArrayOne = []
+    for(i=0;i<array.length-1;i++){
+       newArrayOne.push(array[i])
+    }
+    let final;
+    for (i=0;i<newArrayOne.length;i++){
+       final = doEqual(newArrayOne[i])
+       if(final !== 0){
+          newArrayOne[i]=final.toString();
+       }
+    }
+    results = process(newArrayOne,operator);
+    sumCount =results+operator;
+   if(operator ==="="){
+      sumAll = results
+      first.innerHTML = sumAll;
+   } else {
+      sumAll = results+operator;
+      first.innerHTML = sumAll;
+   }
+}
+const operate= (id) => {
+   let operator="";
+   switch(id.value) {
+      case "clear":
+        first.innerHTML = "";
+        second.innerHTML ="";
+        sumAll=""
+        sumLast="";
+        sumCount=""
+        break;
+      case "plus":
+         operator="+";
+         operatorProcess(operator);
+         break;
+      case "minus":
+         operator="-";
+         operatorProcess(operator);
+         break;
+      case "multiply":
+         operator="x";
+         operatorProcess(operator);
+         break;
+      case 'divide':
+         operator="/";
+         operatorProcess(operator);
+         break;
+      case 'percent':
+         operator="%";
+         operatorProcess(operator);
+            break;
+      case "removeLastItem":
+        sumAll=  sumAll.substring(0,sumAll.length-1)
+        sumLast = sumLast.substring(0,sumLast.length-1)
+        first.innerHTML = sumAll;
+        if(Number.toString(sumCount)){
+            sumCount=  sumCount.substring(0,sumCount.length-1)
+        } else if(Number.isfloat(sumCount)){
+           let newString = sumCount.toString();
+           sumCount=  newString.substring(0,sumCount.length-1)
+           sumCount =parseInt(sumCount);
+        }
+         break;
+      case "equal":
+         operator="=";
+         operatorProcess(operator);
+         break;
+      default:
+       sumAll += id.value;
+       sumCount +=id.value;
+       sumLast +=id.value;//parseInt
+       first.innerHTML = sumAll;
+    }
+}
+const doEqual =(sumArray) => {
+   let result=0;
+   let newArray=[];
+   if(sumArray.includes("+")){
+      newArray = sumArray.split("+")
+      result = process(newArray,"+");
+   } else if(sumArray.includes("-")){
+      newArray = sumArray.split("-");
+      result = process(newArray,"-");
+
+   } else if(sumArray.includes("x")){
+      newArray = sumArray.split("x");
+      result = process(newArray,"x");
+
+   } else if(sumArray.includes("/")){
+      newArray = sumArray.split("/");
+      result = process(newArray,"/");
+
+   } else if(sumArray.includes("%")){
+      newArray = sumArray.split("%");
+      result = process(newArray,"%");
+
+   }
+   return result;
+}
