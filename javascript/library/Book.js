9224780feb02b00942487439c3d7509d718e01a7
Mustafa <cana66331@gmail.com>
2021-06-13 15:13:45 +0200

    some bug fixed and library aplication finished

--- javascript/library/Book.js
+++ javascript/library/Book.js
@@ -23,27 +23,13 @@ function createStyle(){
                  tryArea.classList.add("read")
         }
     }) 
-    
-/*
-    tbodyEl.innerHTML +=
-    `<tr>
-        <td>${i+1}</td>
-        <td>${newArray[i].author}</td>
-        <td>${newArray[i].title}</td>
-        <td>${newArray[i].page}</td>
-        <td>${newArray[i].read}</td>
-        <td><button class="removeItem" id="removeItem" >remove</button></td>
-    </tr>
-    `
-    */
+   
 }
 
 //-------------------get book from localstorage---------------------------
 Book.prototype.getList = function () { 
   let newArray = JSON.parse(localStorage.getItem("library"))
-
     let tbodyEl = document.querySelector("tbody")
-  //  tbodyEl.appendChild(att)
         for(i=0;i<newArray.length;i++){
            
             tbodyEl.innerHTML +=
@@ -52,52 +38,37 @@ Book.prototype.getList = function () {
                         <td>${newArray[i].author}</td>
                         <td>${newArray[i].title}</td>
                         <td>${newArray[i].page}</td>
-                        <td >${newArray[i].read}
-                        ${newArray[i].read==="read"?
-                        "<button class='readButtons' id="+"readButton"+">Read</button>":
-                        "<button class='readButtons' id="+"notReadButton"+">Not</button>"
-                        }
-                        </td>
                         <td ><button class="removeItem" id="removeItem" >remove</button></td>
+                        <td>
+                         <input class='readButtons'  id="+"readButton"  type='checkbox'${newArray[i].read==="read"?'checked':""}>
+                        </td>
                     </tr>
                     `
-         
         }
 }
-
-
-
 
 //-------------------------remove Item in table ------------------------------
 
 
 document.querySelector('#book-list').addEventListener("click",(e)=> {
-
     if(!e.target.classList.contains("removeItem")){
         return;
     } else {
          e.target.parentElement.parentElement.remove();
     }
     let books = JSON.parse(localStorage.getItem("library"))
-    
     if(confirm("Are you sure to remove this item")){
-        
-           // let array =[];
-
-            let content =  e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+            let content =  e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
             console.log("content",content)
             books.forEach((book, index) => {
             if(book.title === content) {
                 if (index > -1) {
-           books.splice(index, 1);
+                    books.splice(index, 1);
                 }
             }
         })
-        localStorage.setItem('library', JSON.stringify(books));
-       
+    localStorage.setItem('library', JSON.stringify(books));
     }
-  
-
 })
 
 //------------------------set ıtem to Arry and localstarage---------------------------
@@ -106,13 +77,8 @@ function insertFunction(){
    const title=document.getElementById('title').value;
    const page = document.getElementById('page').value;
    const read = document.getElementById('status').value;
-   
-let book = new Book(author,title,page,read)
-
-book.addBookToLibrary(book)
+   let book = new Book(author,title,page,read)
+   book.addBookToLibrary(book)
 }
 
 
-
-
-///--------------------------------Store class
--- javascript/library/BookList.js
+++ javascript/library/BookList.js
@@ -5,34 +5,53 @@ book.getList()
 //#notReadButton{
     //------------------------update read status-------------
     document.addEventListener("click",(e)=> {
-        let books = JSON.parse(localStorage.getItem("library"))
-    let content = e.target.textContent
-    console.log("readButton",e.target.textContent)
+    let books = JSON.parse(localStorage.getItem("library"))
+    let contextChexbox = e.target.previousElementSibling
+    let checkboxChecked = e.target.checked
         if(!e.target.classList.contains("readButtons")){
             return;
         }
-            
-    
-        let updateItem =  e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
-        console.log("contentParent",updateItem)
-    
+        let updateItem =  e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
         books.forEach((book) => {
-           
-                if(content==="Not"){
+                if(checkboxChecked===true){
                     if(book.title==updateItem){
                          book.read="read"
+                         contextChexbox = false;
+                    }
+                }else if(checkboxChecked===false){
+                    if(book.title==updateItem){
+                        book.read="not read"
+                        contextChexbox= true;
+                   }
+                }
+        })
+    localStorage.setItem('library', JSON.stringify(books));
+    location.reload()
+    })
+
+
+    
+/*
+ if(content==="Not"){
+                    if(book.title==updateItem){
+                        if(checkboxChecked){
+                             book.read="read"
+                             checkboxChecked= false;
+                        }
+                        
                     }
                            
                 }else if(content==="Read"){
                     if(book.title==updateItem){
-                        book.read="not read"
+                        if(!checkboxChecked){
+                             book.read="not read"
+                        contextChexbox.checked= false;
+                        }
+                       
                    }
                 }
-    
-        })
-        localStorage.setItem('library', JSON.stringify(books));
-   location.reload()
-    })
+
+*/
 
   
 
