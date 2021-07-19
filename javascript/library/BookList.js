039f09125915315fda93bf2846f543ce3a633b7c
Mustafa <cana66331@gmail.com>
2021-07-19 20:12:22 +0200

    class added

--- javascript/library/BookList.js
+++ javascript/library/BookList.js
@@ -1,32 +1,34 @@
 
-
-//const book = new Book()
-//book.getList()
-//#notReadButton{
+ window.addEventListener('DOMContentLoaded', (event) => {
+     event.preventDefault()
+    const book = new Book()
+    book.getList
+});
     //------------------------update read status-------------
     document.addEventListener("click",(e)=> {
-    let books = JSON.parse(localStorage.getItem("library"))
-    let contextChexbox = e.target.previousElementSibling
-    let checkboxChecked = e.target.checked
-        if(!e.target.classList.contains("readButtons")){
-            return;
-        }
-        let updateItem =  e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
-        books.forEach((book) => {
-                if(checkboxChecked===true){
-                    if(book.title==updateItem){
-                         book.read="read"
-                         contextChexbox = false;
+        e.preventDefault()
+        let books = JSON.parse(localStorage.getItem("library"))
+        let contextChexbox = e.target.previousElementSibling
+        let checkboxChecked = e.target.checked
+            if(!e.target.classList.contains("readButtons")){
+                return;
+            }
+            let updateItem =  e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+            books.forEach((book) => {
+                    if(checkboxChecked===true){
+                        if(book.title==updateItem){
+                            book.read="read"
+                            contextChexbox = false;
+                        }
+                    }else if(checkboxChecked===false){
+                        if(book.title==updateItem){
+                            book.read="not read"
+                            contextChexbox= true;
                     }
-                }else if(checkboxChecked===false){
-                    if(book.title==updateItem){
-                        book.read="not read"
-                        contextChexbox= true;
-                   }
-                }
-        })
-    localStorage.setItem('library', JSON.stringify(books));
-   // location.reload()
+                    }
+            })
+        localStorage.setItem('library', JSON.stringify(books));
+        location.reload()
     })
 
 
