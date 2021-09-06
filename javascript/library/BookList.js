1da95132388bb499584b30e0b2e0d20005856797
Mustafa <cana66331@gmail.com>
2021-09-06 10:57:16 +0200

    form validation added

--- javascript/library/BookList.js
+++ javascript/library/BookList.js
@@ -1,38 +1,43 @@
-
- window.addEventListener('DOMContentLoaded', (event) => {
-     event.preventDefault()
-    const book = new Book()
-    book.getList
+window.addEventListener('DOMContentLoaded', (event) => {
+  event.preventDefault();
+  const book = new Book();
+  book.getList;
 });
-    //------------------------update read status-------------
-    document.addEventListener("click",(e)=> {
-        e.preventDefault()
-        let books = JSON.parse(localStorage.getItem("library"))
-        let contextChexbox = e.target.previousElementSibling
-        let checkboxChecked = e.target.checked
-            if(!e.target.classList.contains("readButtons")){
-                return;
-            }
-            let updateItem =  e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
-            books.forEach((book) => {
-                    if(checkboxChecked===true){
-                        if(book.title==updateItem){
-                            book.read="read"
-                            contextChexbox = false;
-                        }
-                    }else if(checkboxChecked===false){
-                        if(book.title==updateItem){
-                            book.read="not read"
-                            contextChexbox= true;
-                    }
-                    }
-            })
-        localStorage.setItem('library', JSON.stringify(books));
-        location.reload()
-    })
+//------------------------update read status-------------
+document.addEventListener('click', (e) => {
+  e.preventDefault();
+  let books = JSON.parse(localStorage.getItem('library'));
+  let contextChexbox = e.target.previousElementSibling;
+  let checkboxChecked = e.target.checked;
+  if (!e.target.classList.contains('readButtons')) {
+    return;
+  }
+  let updateItem =
+    e.target.parentElement.previousElementSibling.previousElementSibling
+      .previousElementSibling.textContent;
+  books.forEach((book) => {
+    if (checkboxChecked === true) {
+      if (book.title == updateItem) {
+        book.read = 'read';
+        contextChexbox = false;
+      }
+    } else if (checkboxChecked === false) {
+      if (book.title == updateItem) {
+        book.read = 'not read';
+        contextChexbox = true;
+      }
+    }
+  });
+  localStorage.setItem('library', JSON.stringify(books));
+  location.reload();
+});
 
+const title = document.getElementById('inputAuthorArea');
 
-
-   
-
-  
+title.addEventListener('input', function (event) {
+  if (title.validity.typeMismatch) {
+    title.setCustomValidity('I am expecting correct input!');
+  } else {
+    title.setCustomValidity('');
+  }
+});
